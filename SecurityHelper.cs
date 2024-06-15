9ee8c53c3b6aff6084af467c9a454840fbeeb722
Sergey Nikitin <sergus12322@yandex.ru>
2024-06-16 01:09:17 +0300

    Added new class SecurityHelper

---
+++ SecurityHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+
+namespace ConnectionApp
+{
+    public static class SecurityHelper
+    {
+        public static SecureString ConvertToSecureString(string str)
+        {
+            SecureString secureStr = new SecureString();
+            foreach (char c in str)
+            {
+                secureStr.AppendChar(c);
+            }
+            secureStr.MakeReadOnly();
+            return secureStr;
+        }
+    }
+}
